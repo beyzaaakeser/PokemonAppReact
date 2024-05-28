1e55eb731331b9632c3a949c428011b0048c0e7e
Beyza Keser <beyzaaakeser@gmail.com>
2024-05-28 19:38:33 +0300

    project completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "./css/Pokecard.css"
+import Pokegame from "./components/Pokegame"
+function App() {
+  return (
+    <div className="App">
+      <h1 className='Game-Title'>Pokemon Go Card Game</h1>
+      <Pokegame/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pokedex.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Pokecard from './Pokecard';
+import '../css/Pokedex.css';
+
+class Pokedex extends Component {
+  render() {
+    let title;
+    if (this.props.isWinner) {
+      title = <h2 className="Pokedex-winner">🎉 Winning Player 🥳 </h2>;
+    } else {
+      title = <h2 className="Pokedex-loser"> 😧 Losing Player 🥺</h2>;
+    }
+
+    return (
+      <div className="Pokedex">
+        {title}
+        <h4>Total Experience: {this.props.exp}</h4>
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => (
+            <Pokecard
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Pokedex;
